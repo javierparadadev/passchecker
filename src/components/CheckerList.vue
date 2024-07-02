07027b06cdbfbac93e7e370c5209b88ac542fca1
Javier Parada <javierparada@pm.me>
2024-07-02 15:21:09 +0900

    add another sample

--- src/components/CheckerList.vue
+++ src/components/CheckerList.vue
@@ -20,22 +20,13 @@ import ToolingIcon from './icons/IconTooling.vue'
     <template #icon>
       <ToolingIcon />
     </template>
-    <template #heading>Tooling</template>
+    <template #heading>zxcvbn</template>
 
-    This project is served and bundled with
-    <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
-    recommended IDE setup is
-    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a> +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener">Volar</a>. If
-    you need to test your components and web pages, check out
-    <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a> and
-    <a href="https://on.cypress.io/component" target="_blank" rel="noopener"
-      >Cypress Component Testing</a
-    >.
+    Algorithm, developed by <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener">Dropbox</a>, 
+    assesses password strength by estimating the number of attempts 
+    needed for an attacker to guess it. It evaluates against common passwords, keyboard patterns, demographic data, 
+    and accounts for both offline and online attack scenarios.
 
-    <br />
-
-    More instructions are available in <code>README.md</code>.
   </PassCheckerItem>
 
 </template>
